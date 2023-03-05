a9044dab3ac51e0d51088287ad060ef6e0f02b71
Alex Nagy <rebeloperalpar@gmail.com>
2023-03-05 07:12:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# kindaSwiftUI
+
+## 10x Your SwiftUI Development
+
+Documentation is currently work in progress 😁
+Stay tuned...
---
+++ Sources/kindaSwiftUI.xcframework/tvos-arm64_x86_64-simulator/kindaSwiftUI.framework/Headers/kindaSwiftUI.h
@@ -0,0 +1,18 @@
+//
+//  kindaSwiftUI.h
+//  kindaSwiftUI
+//
+//  Created by Alex Nagy on 17.02.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kindaSwiftUI.
+FOUNDATION_EXPORT double kindaSwiftUIVersionNumber;
+
+//! Project version string for kindaSwiftUI.
+FOUNDATION_EXPORT const unsigned char kindaSwiftUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kindaSwiftUI/PublicHeader.h>
+
+
